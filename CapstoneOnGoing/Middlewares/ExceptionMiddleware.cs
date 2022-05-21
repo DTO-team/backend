f784fd354918361dd8f5c7f866c33ae3caf62215
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-05-21 17:18:52 +0700

    DTO-67 Setup NLog and Global Error Handling (#1)
    * Test
    
    * Revert "Test"
    
    This reverts commit 13f8086550f92e69e75854f2430c95f6e3013ce4.
    
    * Revert "Revert "Test""
    
    This reverts commit 81d772243111864685a2053ea47c5b47834603d3.
    
    * Revert "Revert "Revert "Test"""
    
    This reverts commit 3233310717bb63c9c7f9e726995eb18c29f7b171.
    
    * Test Push
    
    * Adjust gitignore
    
    * DTO-67 Setup Nlog and Global Error Handling
    
    * DTO-67 Done set up Nlog and Global Error Handling
    
    * Update LoggerManager.cs
    
    * Update Startup.cs
    
    Co-authored-by: dat-dang <dat.dang@billidentity.com>

---
+++ CapstoneOnGoing/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using CapstoneOnGoing.Logger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Models;
+
+namespace CapstoneOnGoing.Middlewares
+{
+	public static class ExceptionMiddleware
+	{
+		public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILoggerManager logger)
+		{
+			app.UseExceptionHandler(appError =>
+			{
+				appError.Run(async context =>
+				{
+					context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+					context.Response.ContentType = "application/json";
+
+					IExceptionHandlerFeature contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+					if (contextFeature != null)
+					{
+						logger.LogError($"Error: {contextFeature.Error}");
+						await context.Response.WriteAsync(new ErrorDetails()
+						{
+							StatusCode = context.Response.StatusCode,
+							Details = contextFeature.Error,
+							Message = contextFeature.Error.Message,
+							TimeStamp = DateTime.Now
+						}.ToString());
+					}
+				});
+			});
+		}
+	}
+}

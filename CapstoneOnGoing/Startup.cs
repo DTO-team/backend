b44459135146fee11925a9243f997dcf8abd0392
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-05-21 17:18:03 +0700

    Update Startup.cs

--- CapstoneOnGoing/Startup.cs
+++ CapstoneOnGoing/Startup.cs
@@ -31,7 +31,7 @@ namespace CapstoneOnGoing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-			services.AddSingleton<ILoggerManager, LoggerManager>();
+	    services.AddSingleton<ILoggerManager, LoggerManager>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {

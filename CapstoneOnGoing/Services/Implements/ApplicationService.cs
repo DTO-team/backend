2b8aedb1fd94e0aed1ba198b3f5a4bcc2f7ab47e
Dong Quan <dongquan1212@gmail.com>
2022-05-23 13:21:34 +0700

    Create Service Folder

---
+++ CapstoneOnGoing/Services/Implements/ApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using CapstoneOnGoing.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstoneOnGoing.Services.Implements
+{
+    public class ApplicationService: IApplicationService
+    {
+
+    }
+}

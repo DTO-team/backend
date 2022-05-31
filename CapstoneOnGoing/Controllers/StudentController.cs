78210ab4284584a36c3cb55ba07a32a373a420b0
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-05-31 16:33:28 +0700

    Add Health Check (#12)
    * Refactor code
    
    * DTO-54 Refactor Repository and Implement AutoMapper
    
    * Adjust Retreive Connection String and Remove API Gateway
    
    * Add Retrieve connection string from environment variables
    
    * DTO-54 Add verify access token
    
    * Add health check
    
    * Add Health Check

--- CapstoneOnGoing/Controllers/StudentController.cs
+++ CapstoneOnGoing/Controllers/StudentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Repository.Interfaces;
 using System;
--- CapstoneOnGoing/Startup.cs
+++ CapstoneOnGoing/Startup.cs
@@ -18,11 +18,16 @@ using NLog;
 using Repository;
 using Microsoft.EntityFrameworkCore;
 using CapstoneOnGoing.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace CapstoneOnGoing
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             var builder = new ConfigurationBuilder()
@@ -39,12 +44,40 @@ namespace CapstoneOnGoing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => {
+                options.AddPolicy(name: MyAllowSpecificOrigins, 
+                                    policy => {
+                                        policy.WithOrigins("dto.codes","localhost");
+                                    });
+            });
             services.AddAutoMapper(typeof(Startup));
             var connectionString = $"Server={Configuration.GetValue<string>("SERVER")},{Configuration.GetValue<string>("PORT")};User Id={Configuration.GetValue<string>("USERID")};" +
                 $"Password={Configuration.GetValue<string>("PASSWORD")};Database={Configuration.GetValue<string>("DATABASE")};";
             services.AddDbContext<CAPSTONEONGOINGContext>(options => options.UseSqlServer(connectionString));
             services.AddSingleton<ILoggerManager, LoggerManager>();
             services.AddRepository();
+            //Valid Access Token
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options =>
+                    {
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            IssuerSigningKeyResolver = (s, securityToken, identifier, parameters) =>
+                            {
+                                //get JsonWebKeySet from AWS
+                                var json = new WebClient().DownloadString(parameters.ValidIssuer + "/.well-known/jwks.json");
+                                //seriablize the result 
+                                var keys = JsonConvert.DeserializeObject<JsonWebKeySet>(json).Keys;
+                                return (IEnumerable<SecurityKey>)keys;
+                            },
+                            ValidIssuer = $"https://cognito-idp.{Configuration.GetValue<string>("REGION")}.amazonaws.com/{Configuration.GetValue<string>("POOLID")}",
+                            ValidateIssuerSigningKey = true,
+                            ValidateIssuer = true,
+                            ValidateLifetime = true,
+                            ValidAudience = "{Cognito AppClientID}",
+                            ValidateAudience = true,
+                        };
+                    });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -61,12 +94,12 @@ namespace CapstoneOnGoing
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CapstoneOnGoing v1"));
             }
-
             app.ConfigureExceptionHandler(logger);
             app.UseMiddleware<RequestLoggingMiddleware>();
-            app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseAuthorization();
 

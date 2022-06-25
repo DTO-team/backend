41b6081944acb5a61922c460ba1fe8d71a5afaa7
Đặng Xuân Đạt <68004908+dangxuandat@users.noreply.github.com>
2022-06-25 15:59:10 +0700

    Dto 40 fix get topic response error (#65)
    * Fix mapping Get topics response
    
    * DTO-40 Adjust lecturer reponse
    
    * DTO-40 Add Department in Lectuerer reponse
    
    * DTO-40 Fix get topic errror

---
+++ CapstoneOnGoing/Services/Implements/ReportService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoMapper;
+using CapstoneOnGoing.Logger;
+using CapstoneOnGoing.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Models.Dtos;
+using Models.Models;
+using Models.Request;
+using Repository.Interfaces;
+
+namespace CapstoneOnGoing.Services.Implements
+{
+	public class ReportService : IReportService
+	{
+		private readonly ILoggerManager _logger;
+		private readonly IMapper _mapper;
+		private readonly IUnitOfWork _unitOfWork;
+		public ReportService(ILoggerManager logger, IMapper mapper, IUnitOfWork unitOfWork)
+		{
+			_logger = logger;
+			_mapper = mapper;
+			_unitOfWork = unitOfWork;
+		}
+		public bool CreateWeeklyReport(Guid teamId, string studentEmail, CreateWeeklyReportDTO createWeeklyReportDTO)
+		{
+			//check team is exist
+			Team team = _unitOfWork.Team.GetById(teamId);
+			if (team == null)
+			{
+				throw new BadHttpRequestException("Team does not exist");
+			}
+			return true;
+		}
+	}
+}

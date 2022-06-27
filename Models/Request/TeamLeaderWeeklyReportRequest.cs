1d75e772e1ebba484c0331cb3d92e4be005a930c
Dong Quan <dongquan1212@gmail.com>
2022-06-27 22:22:18 +0700

    set up request for leader create report

---
+++ Models/Request/TeamLeaderWeeklyReportRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.Request
+{
+    public class TeamLeaderWeeklyReportRequest
+    {
+        [Required]
+        public Guid ProjectId { get; set; }
+        [Required]
+        public Guid ReporterId { get; set; }
+        public Guid WeekId { get; set; }
+        public bool IsTeamReport { get; set; }
+        public long Deadline { get; set; }
+        public string CompletedTasks { get; set; }
+        public string InProgressTasks { get; set; }
+        public string NextWeekTasks { get; set; }
+        public string UrgentIssues { get; set; }
+        public string SelfAssessments { get; set; }
+        public string Feedback { get; set; }
+    }
+}

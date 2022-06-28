2bd8d4582d7c634b2a4ff5b364814700abafe3dd
Dong Quan <dongquan1212@gmail.com>
2022-06-28 13:32:13 +0700

    fix request data for update lecturer

--- Models/Request/UpdateLecturerRequest.cs
+++ Models/Request/UpdateLecturerRequest.cs
@@ -5,12 +5,9 @@ namespace Models.Dtos
 {
     public class UpdateLecturerRequest
     {
-        [Required]
-        public Guid Id { get; set; }
-        public string Email { get; set; }
         public string UserName { get; set; }
         public string FullName { get; set; }
         public string AvatarUrl { get; set; }
-
+        public Guid DepartmentId { get; set; }
     }
 }

c63e56134074fa30debfc4f2ed8830932d1506a0
Đặng Đông Quân <74441708+quandangdong@users.noreply.github.com>
2022-06-28 13:57:09 +0700

    fix request data for update lecturer (#75)

--- CapstoneOnGoing/Services/Interfaces/ILecturerService.cs
+++ CapstoneOnGoing/Services/Interfaces/ILecturerService.cs
@@ -11,7 +11,7 @@ namespace CapstoneOnGoing.Services.Interfaces
         void CreateLecturer(Lecturer lecturer, Guid userId, Guid departmentId);
         IEnumerable<User> GetAllLecturers(int page, int limit);
         User GetLecturerById(Guid lecturerId);
-        User UpdateLecturer(User lecturerToUpdate);
+        User UpdateLecturer(Guid lecturerId, UpdateLecturerRequest lecturerToUpdate);
         User GetLecturerByEmail(string userEmail);
         void DeleteLecturer(Lecturer lecturerToDelete);
         void DeleteLecturerById(Guid lecturerId);

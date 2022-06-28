7e98dea9521797606b238fa275d92696bf4a0a21
Dong Quan <dongquan1212@gmail.com>
2022-06-28 09:21:31 +0700

    process for create weekly report

--- CapstoneOnGoing/Services/Implements/ReportService.cs
+++ CapstoneOnGoing/Services/Implements/ReportService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using CapstoneOnGoing.Enums;
@@ -13,70 +14,119 @@ using Repository.Interfaces;
 
 namespace CapstoneOnGoing.Services.Implements
 {
-	public class ReportService : IReportService
-	{
-		private readonly ILoggerManager _logger;
-		private readonly IMapper _mapper;
-		private readonly IUnitOfWork _unitOfWork;
-		public ReportService(ILoggerManager logger, IMapper mapper, IUnitOfWork unitOfWork)
-		{
-			_logger = logger;
-			_mapper = mapper;
-			_unitOfWork = unitOfWork;
-		}
-		public bool CreateWeeklyReport(Guid teamId, string studentEmail, CreateWeeklyReportDTO createWeeklyReportDTO)
-		{
-			//check team is exist
-			Semester currentSemester = _unitOfWork.Semester.Get(x => x.Status == (int)SemesterStatus.Ongoing).FirstOrDefault();
-			Team team = _unitOfWork.Team.Get( x => (x.Id == teamId && x.SemesterId == currentSemester.Id),null, "TeamStudents").FirstOrDefault();
-			Project currentProject = _unitOfWork.Project.Get(x => x.TeamId == team.Id).FirstOrDefault();
-			User user = _unitOfWork.User.Get(x => x.Email.Equals(studentEmail)).FirstOrDefault();
-			if (team == null)
-			{
-				throw new BadHttpRequestException("Team does not exist");
-			}
-			if (team.TeamStudents.Select(x => x.StudentId).Contains(user.Id))
-			{
-				//Get current week
-				long currentDateTime = DateTimeHelper.ConvertDateTimeToLong(DateTime.Now);
-				Week currentWeek = _unitOfWork.Week
-					.Get(x => x.FromDate <= currentDateTime && x.ToDate >= currentDateTime).FirstOrDefault();
-				//check deadline
-				if (currentDateTime >= currentWeek.Deadline)
-				{
-					throw new BadHttpRequestException(
-						$"Deadline of week was: {DateTimeHelper.ConvertLongToDateTime(currentWeek.Deadline)}");
-				}
-				//
-				Report newReport = new Report()
-				{
-					ProjectId = currentProject.Id,
-					ReporterId = user.Id,
-					IsTeamReport = createWeeklyReportDTO.IsTeamReport,
-					CompletedTasks = createWeeklyReportDTO.CompletedTasks,
-					InProgressTasks = createWeeklyReportDTO.InProgressTasks,
-					NextWeekTasks = createWeeklyReportDTO.NextWeekTasks,
-					UrgentIssues = createWeeklyReportDTO.UrgentIssues,
-					SelfAssessments = createWeeklyReportDTO.SelfAssessment,
-					Feedbacks = createWeeklyReportDTO.FeedBack,
-					WeekId = currentWeek.Id
-				};
-				Array.ForEach(createWeeklyReportDTO.ReportEvidences.ToArray(), reportEvidence =>
-				{
-					newReport.ReportEvidences.Add(new ReportEvidence()
-					{
-						Url = reportEvidence.Url,
-						Name = reportEvidence.Name,
-						ReportId = newReport.Id
-					});
-				});
-				_unitOfWork.Report.Insert(newReport);
-				return _unitOfWork.Save() > 0;
-			}
-			else
-			{
-				throw new BadHttpRequestException("You're not in the team");
-			}
-		}
-	}
+    public class ReportService : IReportService
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ReportService(ILoggerManager logger, IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        private bool CreateWeeklyReport(Project currentProject, User user, CreateWeeklyReportDTO createWeeklyReportDTO,
+            Week currentWeek, long currentDateTime)
+        {
+
+            //check deadline
+            if (currentDateTime >= currentWeek.Deadline)
+            {
+                throw new BadHttpRequestException(
+                    $"Deadline of week was: {DateTimeHelper.ConvertLongToDateTime(currentWeek.Deadline)}");
+            }
+
+            //
+            Report newReport = new Report()
+            {
+                ProjectId = currentProject.Id,
+                ReporterId = user.Id,
+                IsTeamReport = createWeeklyReportDTO.IsTeamReport,
+                CompletedTasks = createWeeklyReportDTO.CompletedTasks,
+                InProgressTasks = createWeeklyReportDTO.InProgressTasks,
+                NextWeekTasks = createWeeklyReportDTO.NextWeekTasks,
+                UrgentIssues = createWeeklyReportDTO.UrgentIssues,
+                SelfAssessments = createWeeklyReportDTO.SelfAssessment,
+                Feedbacks = createWeeklyReportDTO.FeedBack,
+                WeekId = currentWeek.Id
+            };
+            Array.ForEach(createWeeklyReportDTO.ReportEvidences.ToArray(), reportEvidence =>
+            {
+                newReport.ReportEvidences.Add(new ReportEvidence()
+                {
+                    Url = reportEvidence.Url,
+                    Name = reportEvidence.Name,
+                    ReportId = newReport.Id
+                });
+            });
+            _unitOfWork.Report.Insert(newReport);
+            return _unitOfWork.Save() > 0;
+        }
+
+
+        public bool CreateWeeklyReport(Guid teamId, string studentEmail, CreateWeeklyReportDTO createWeeklyReportDTO)
+        {
+            //check team is exist
+            Semester currentSemester =
+                _unitOfWork.Semester.Get(x => x.Status == (int)SemesterStatus.Ongoing).FirstOrDefault();
+            Team team = _unitOfWork.Team
+                .Get(x => (x.Id == teamId && x.SemesterId == currentSemester.Id), null, "TeamStudents")
+                .FirstOrDefault();
+            Project currentProject = _unitOfWork.Project.Get(x => x.TeamId == team.Id).FirstOrDefault();
+            User user = _unitOfWork.User.Get(x => x.Email.Equals(studentEmail)).FirstOrDefault();
+            if (team == null)
+            {
+                throw new BadHttpRequestException("Team does not exist");
+            }
+
+            if (team.TeamStudents.Select(x => x.StudentId).Contains(user.Id))
+            {
+                //Get current week
+                long currentDateTime = DateTimeHelper.ConvertDateTimeToLong(DateTime.Now);
+                Week currentWeek = _unitOfWork.Week
+                    .Get(x => x.FromDate <= currentDateTime && x.ToDate >= currentDateTime).FirstOrDefault();
+
+                //Check is any report exist by reporter and currentWeek;
+                IEnumerable<Report> reports =
+                    _unitOfWork.Report.Get(report =>
+                        report.ReporterId.Equals(user.Id) && report.WeekId.Equals(currentWeek.Id));
+
+                //If not have any exist => create new report
+                if (!reports.Any())
+                {
+                    bool isCreated = CreateWeeklyReport(currentProject, user, createWeeklyReportDTO, currentWeek, currentDateTime);
+                    return isCreated;
+                }
+
+                //If have => check is team report or not (because of team leader can create 2 report)
+                //If not team report => create new team report 
+                else if (reports.Any())
+                {
+                    bool isExistedReport = reports.ToArray().Select(report =>
+                            report.IsTeamReport.Equals(createWeeklyReportDTO.IsTeamReport))
+                        .FirstOrDefault();
+                    if (!isExistedReport)
+                    {
+                        bool isCreated = CreateWeeklyReport(currentProject, user, createWeeklyReportDTO, currentWeek, currentDateTime);
+                        return isCreated;
+                    }
+                    else
+                    {
+                        throw new BadHttpRequestException("You already update your team report in this week");
+                    }
+                }
+                else
+                {
+                    throw new BadHttpRequestException("You already update your report in this week");
+                }
+            }
+            else
+            {
+                throw new BadHttpRequestException("You're not in the team");
+            }
+        }
+    }
 }
+

9932948db07ceb2530deb24f29b49727908feaf0
Dong Quan <dongquan1212@gmail.com>
2022-07-06 02:05:09 +0700

    fix CICD error

--- Models/Response/GetTeamDetailResponse.cs
+++ Models/Response/GetTeamDetailResponse.cs
@@ -5,9 +5,7 @@ namespace Models.Response
 {
 	public class GetTeamDetailResponse : GetTeamResponse
 	{
-        public Guid ProjectId { get; set; }
-        public bool IsApplicationApproved { get; set; }
-		public IList<GetLecturerResponse> Mentors { get; set; }
+        public IList<GetLecturerResponse> Mentors { get; set; }
 		public IList<Member> Members { get; set; }
 	}
 

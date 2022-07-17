64b843a629c563b2f800500b285128a44163df82
Đặng Đông Quân <74441708+quandangdong@users.noreply.github.com>
2022-07-17 17:05:38 +0700

    Dto-158: add api for delete criteria, create new criteria question, grade (#115)
    * set up for delete criteria api
    
    * api for delele, create new grade,question for criteria
    
    * add authorize for api

---
+++ Models/Request/CreateNewCriteriaGradeRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models.Request
+{
+    public class CreateNewCriteriaGradeRequest
+    {
+        public string Level { get; set; }
+        public int MinPoint { get; set; }
+        public int MaxPoint { get; set; }
+        public string Description { get; set; }
+    }
+}

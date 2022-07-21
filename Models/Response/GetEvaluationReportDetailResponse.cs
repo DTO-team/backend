3e81fcae9024d905b44b8689daab95f873a5ba15
Đặng Đông Quân <74441708+quandangdong@users.noreply.github.com>
2022-07-21 11:52:45 +0700

    Dto-166: api for get all and by id evaluation report  (#141)
    * api for create evaluation report
    
    * api for update evaluation report
    
    * api for get all evaluation report and detail by id

---
+++ Models/Response/GetEvaluationReportDetailResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Models.Models;
+
+namespace Models.Response
+{
+    public class GetEvaluationReportDetailResponse
+    {
+        public GetEvaluationSessionResponse EvaluationSession { get; set; }
+        public EvaluationReportDetailResponse EvaluationReportDetail { get; set; }
+        public GetTeamDetailResponse TeamDetail { get; set; }
+    }
+}
